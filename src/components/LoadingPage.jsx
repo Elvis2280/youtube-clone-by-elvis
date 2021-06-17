3999ecce5b21a6677857bdcf61d71231732ed72e
Elvis Miranda <elvismiranda924g@gmail.com>
2021-06-17 11:00:43 -0500

    test2

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -8,10 +8,10 @@ const LoadingPage = (props) => {
   return promiseInProgress ? (
     <div class="fixed top-0 right-0 h-screen w-screen z-50 flex flex-col justify-center items-center">
       <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-red-600"></div>
-      <p className="text-green-400 pt-3">Loading...</p>
+      <p className="text-black pt-3">Loading...</p>
     </div>
   ) : (
-    <div className="col-span-12 flex flex-col items-center transform -translate-y-2/4 absolute top-2/4">
+    <div className="col-span-12 flex flex-col items-center transform -translate-y-2/4 absolute top-0">
       <img
         className="h-auto w-8/12 sm:h-2/4 sm:w-2/4"
         src={image}
